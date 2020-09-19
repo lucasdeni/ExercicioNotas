71f4a5e5bfeda193bc3ff3ce3e15ecf92567307a
Lucas Denipoti <lucasfdenipoti@gmail.com>
2020-09-19 10:43:51 -0300

    Exercício 3 Finalizado

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Aluno
+    {
+        public static string Nome;
+        public static double Nota1;
+        public static double Nota2;
+        public static double Nota3;
+
+        public static double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+        public static double Resto()
+        {
+            return 60 - (Nota1 + Nota2 + Nota3);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Aluno: ");
+            Aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as notas do aluno:");
+            Aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double notaFinal = Aluno.NotaFinal();
+            Console.WriteLine("Nota Final = " + notaFinal.ToString("F2", CultureInfo.InvariantCulture));
+
+            double resto = Aluno.Resto();
+            if(notaFinal < 60)
+            {
+                Console.WriteLine("Reprovado");
+                Console.WriteLine("Faltam " + resto.ToString("F2", CultureInfo.InvariantCulture) + " Pontos");
+
+            }
+            else if(notaFinal >= 60)
+            {
+                Console.WriteLine("Aprovado");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
